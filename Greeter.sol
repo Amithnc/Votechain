cc9f83476c0a8343fff8d61163abc2cb294c374e
Amithnc <53530365+Amithnc@users.noreply.github.com>
2020-10-15 02:20:52 +0530

    added a simple voting contract.
    :beginner:

--- Greeter.sol
+++ Greeter.sol
@@ -1,18 +1,61 @@
-pragma solidity ^0.4.21;
+pragma solidity 0.4.20;
 
+contract Election {
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
 
-contract Greeter{
-    string public greeting;
+address public chairperson;
     
-    
-    function Greeter() public{
-        greeting="hello";
+    mapping(address=>bool)public status;
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+    function Election() public{
+        chairperson = msg.sender;
+    }
+
+    function addCandidate (string _name) public {
+        require(msg.sender == chairperson);
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
     
-    function setGreeting(string _greeting) public{
-        greeting=_greeting;
+    function approve (address _address) public {
+        require(
+            msg.sender == chairperson);
+        status[_address]=true;
     }
-    function greet() view public returns (string){
-        return greeting;
-    } 
+  
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(status[msg.sender]);
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        votedEvent(_candidateId);
+    }
 }
--- test.py
+++ test.py
@@ -4,19 +4,26 @@ import json
 ganache_url="HTTP://127.0.0.1:7545"
 web3=Web3(Web3.HTTPProvider(ganache_url))
 
-web3.eth.defaultAccount=web3.eth.accounts[0]
+web3.eth.defaultAccount=web3.eth.accounts[2]
 
 
-abi=json.loads('[{"constant":false,"inputs":[{"name":"_greeting","type":"string"}],"name":"setGreeting","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"greet","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"greeting","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]')
-address=web3.toChecksumAddress("0xA28156d0932b346c6BdA91c19179513ed2d6BeC4")
+abi=json.loads('[{"constant":false,"inputs":[{"name":"_candidateId","type":"uint256"}],"name":"vote","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"candidatesCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidates","outputs":[{"name":"id","type":"uint256"},{"name":"name","type":"string"},{"name":"voteCount","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"voters","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_candidateId","type":"uint256"}],"name":"votedEvent","type":"event"}]')
+address=web3.toChecksumAddress("0xA45742fDD28eF56c736033e53B9e567dF203F910")
 
 contract=web3.eth.contract(address=address,abi=abi)
-print(contract.functions.greet().call())
+try:
+  print(contract.functions.vote(1).transact())
+except:
+  print("you have already voted")
 
-tx_hash=contract.functions.setGreeting('HELLO WORLD').transact()
+count=contract.functions.candidatesCount().call()
+for i in range(1,count+1):
+  party=(contract.functions.candidates(i).call())
+  print(party)
+# tx_hash=contract.functions.setGreeting('HELLO WORLD').transact()
 
-web3.eth.waitForTransactionReceipt(tx_hash)
+# web3.eth.waitForTransactionReceipt(tx_hash)
 
-print("updated greeting {}".format(
-  contract.functions.greet().call()
-  ))
+# print("updated greeting {}".format(
+#   contract.functions.greet().call()
+#   ))
