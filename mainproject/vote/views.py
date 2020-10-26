4ee90fea9074beaf15d7bf41ab5f6e0b83a5be94
Amithnc <53530365+Amithnc@users.noreply.github.com>
2020-10-26 23:57:32 +0530

    started building the dapp
    created urls to the vote app

--- mainproject/vote/views.py
+++ mainproject/vote/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from web3 import Web3
+import json
 
 # Create your views here.
+    ganache_url="HTTP://127.0.0.1:7545"
+    web3=Web3(Web3.HTTPProvider(ganache_url))
+
+    return HttpResponse('Homepage')
