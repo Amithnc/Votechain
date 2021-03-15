ea7ee77b221efcfd1a2df4fbf11787a728c0769e
Amithnc <53530365+Amithnc@users.noreply.github.com>
2021-03-16 04:13:57 +0530

    added form validation and cookie feature.

---
+++ mainproject/statics/js/register.js
@@ -0,0 +1,58 @@
+setTimeout(function () {
+    // Closing the alert 
+    $('#alert').alert('close');
+}, 8000);
+function validateForm() {
+    name = document.notesForm.name.value
+    email = document.notesForm.email.value
+    phone = document.notesForm.phone.value;
+    aadhar = document.notesForm.aadhar.value;
+    password = document.notesForm.password.value;
+    cnfpassword = document.notesForm.cnfpassword.value;
+    //set cookie.....
+    var d = new Date();
+    d.setTime(d.getTime() + (30 * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = "phone" + "=" + phone + ";" + expires + ";path=/";
+    document.cookie = "aadhar" + "=" + aadhar + ";" + expires + ";path=/";
+    document.cookie = "name" + "=" + name + ";" + expires + ";path=/";
+    document.cookie = "email" + "=" + email + ";" + expires + ";path=/";
+    //end set cookie...
+    if (isNaN(phone)) {
+        alertify.alert('❌ERROR', "Please enter correct phone number", function () { alertify.error('Enter Correct Phone Number'); });
+        return false
+    }
+    if (isNaN(aadhar)) {
+        alertify.alert('❌ERROR', "Please enter correct aadhar number", function () { alertify.error('Enter Correct Aadhar Number'); });
+        return false
+    }
+    if (password != cnfpassword) {
+        alertify.alert('❌ERROR', "Both passwords doesnt match", function () { alertify.error('Enter Correct passwords'); });
+        return false
+    }
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+function checkCookie() {
+    var user = getCookie("name");
+    var phone = getCookie("phone");
+    var aadhar= getCookie("aadhar"); 
+    var email= getCookie("email");
+    document.getElementById("name").value = user;
+    document.getElementById("phone").value = phone;
+    document.getElementById("aadhar").value = aadhar;
+    document.getElementById("email").value = email;
+}
--- mainproject/vote/views.py
+++ mainproject/vote/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
 from django.http import HttpResponse
 from web3 import Web3
 import json
@@ -9,6 +10,29 @@ def homepage(request):
     return render(request,'home.html')
 
 def register(request):
+    if request.method == "POST":
+        aadhar=request.POST.get('aadhar',None)
+        name=request.POST.get('name',None)
+        email=request.POST.get('email',None)
+        password=request.POST.get('password',None)
+        phone=request.POST.get('phone',None)
+        raw_url="http://127.0.0.1:7000/data/"
+        url=raw_url+str(aadhar)
+        response = requests.request("GET", url)
+        temp=eval(response.text)
+        if not temp:
+            messages.warning(request, 'Aadhar card not found. Please check the aadhar number')
+            return redirect('/register')
+        data=temp[0]
+        if data['name'].lower()!=name.lower():
+            messages.warning(request, 'Name doest match with Aadhar database')
+            return redirect('/register')
+        if data['phone_number']!=phone:
+            messages.warning(request, 'Phone Number doest match with aadhar database')
+            return redirect('/register')
+        if data['age']<18:
+            messages.warning(request, 'sorry you cannot cast vote in this age')
+            return redirect('/register')
     return render(request,'register.html')
 
 def capture_images(request):
