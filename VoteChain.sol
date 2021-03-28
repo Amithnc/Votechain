77d6ab489728ea5c893265e89d5c4212f9cc121a
Amithnc <53530365+Amithnc@users.noreply.github.com>
2021-03-28 18:19:06 +0530

    added another field to candidate structure.

--- VoteChain.sol
+++ VoteChain.sol
@@ -4,14 +4,12 @@ contract Election {
     // Model a Candidate
     struct Candidate {
         uint id;
-        string name;
+        string party;
+        string candidate_name;
         uint voteCount;
     }
-
 address public chairperson;
     
-    mapping(address=>bool)public status;
-
     // Store accounts that have voted
     mapping(address => bool) public voters;
     // Store Candidates
@@ -28,22 +26,15 @@ address public chairperson;
         chairperson = msg.sender;
     }
 
-    function addCandidate (string _name) public {
+    function addCandidate (string _party,string _name) public {
         require(msg.sender == chairperson);
         candidatesCount ++;
-        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+        candidates[candidatesCount] = Candidate(candidatesCount, _party, _name, 0);
     }
     
-    function approve (address _address) public {
-        require(
-            msg.sender == chairperson);
-        status[_address]=true;
-    }
   
-
     function vote (uint _candidateId) public {
         // require that they haven't voted before
-        require(status[msg.sender]);
         require(!voters[msg.sender]);
 
         // require a valid candidate
